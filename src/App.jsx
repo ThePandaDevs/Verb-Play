5f05d4f75d28d17e3904777c17bfe6d0af5c754f
JairDavid <20193tn071@utez.edu.mx>
2022-11-01 22:41:55 -0600

    add dashboard page and its components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Dashboard from './pages/Dashboard';
+import './index.css'
 
 // Aqui solamente ira el router una vez que este listo
 // No importar nada
 
 const App = () =>{
   return(
-    <></>
+    <>
+    <Dashboard/>
+    </>
   )
 }
 
