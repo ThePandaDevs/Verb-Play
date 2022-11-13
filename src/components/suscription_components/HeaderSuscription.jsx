96e9e6b39ede6093890d1dd1eec97b8157579f16
EsmeraldaLara <20193tn014@utez.edu.mx>
2022-11-13 16:20:54 -0600

    feat: subscription page and components added

---
+++ src/components/suscription_components/HeaderSuscription.jsx
@@ -0,0 +1,9 @@
+export const HeaderSuscription = () => {
+    return (
+        <>
+            <div className="bg-[#00A67E] w-[100%] py-10 rounded-xl drop-shadow-xl">
+                <p className="text-2xl text-[#ffff] font-semibold tracking-wide">SELECT YOUR PLAN</p>
+            </div>
+        </>
+    )
+}
---
+++ src/pages/SuscriptionPage.jsx
@@ -0,0 +1,11 @@
+import { HeaderSuscription } from "../components/suscription_components/HeaderSuscription"
+import { SuscriptionModes } from "../components/suscription_components/SuscriptionModes"
+
+export const SuscriptionPage = () => {
+    return (
+        <>
+            <HeaderSuscription />
+            <SuscriptionModes />
+        </>
+    )
+} 
